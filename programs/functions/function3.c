9dede1ba97ea4fb6da25f355f86abb47599188ec
bimanmandal <biman29@gmail.com>
2025-05-13 09:19:36 +0530

    dynamic number

--- programs/functions/function3.c
+++ programs/functions/function3.c
@@ -9,6 +9,11 @@ int getNumber(){
     return x;
 }
 
+// Function with 0 parameter and return type int
+int getConstantNumber(){
+    return 32;
+}
+
 // Function with 1 parameter and return type void
 void printResult(int res) {
     printf("The result is %d \n",res);
