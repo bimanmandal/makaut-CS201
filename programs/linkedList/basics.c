20aefabe58aca9c1296dc197c3f4fcfd57929770
bimanmandal <biman29@gmail.com>
2025-06-28 09:21:23 +0530

    added linked list deletion

--- programs/linkedList/basics.c
+++ programs/linkedList/basics.c
@@ -29,6 +29,34 @@ void insertNode(struct Node** head, int data) {
 
 }
 
+
+void deleteNode(struct Node** head, int data) {
+    struct Node* current = *head;
+    struct Node* previous = NULL;
+
+    if (current != NULL && current->data == data ) {
+        *head = current->next;
+        free(current);
+        return;
+    }
+
+    while (current != NULL && current->data != data) {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Node not found\n");
+        return;
+    }
+
+    previous->next = current->next;
+    free(current);
+
+
+
+}
+
 void display(struct Node* head) {
     struct Node* current = head;
     printf("\n Linked List \n");
@@ -44,6 +72,16 @@ int main() {
     insertNode(&head, 10);
     insertNode(&head, 20);
     insertNode(&head, 30);
+    insertNode(&head, 40);
+    insertNode(&head, 50);
+    insertNode(&head, 60);
+    insertNode(&head, 70);
+    display(head);
+    deleteNode(&head, 10);
+    display(head);
+    deleteNode(&head, 70);
+    display(head);
+    deleteNode(&head, 40);
     display(head);
     return 0;
 }
